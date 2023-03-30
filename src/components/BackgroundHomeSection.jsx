f98276514d10f21224b76f6ab26334d73c26a697
Emilien Gerbouin <emilien.gerbouin@gmail.com>
2023-03-30 15:45:15 +0200

    animation background

---
+++ src/components/BackgroundHomeSection.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const BackgroundHomeSection = () => {
+  return (
+    <div className="snow__container">
+      <div class="box-of-star1">
+    <div class="star star-position1"></div>
+    <div class="star star-position2"></div>
+    <div class="star star-position3"></div>
+    <div class="star star-position4"></div>
+    <div class="star star-position5"></div>
+    <div class="star star-position6"></div>
+    <div class="star star-position7"></div>
+  </div>
+  <div class="box-of-star2">
+    <div class="star star-position1"></div>
+    <div class="star star-position2"></div>
+    <div class="star star-position3"></div>
+    <div class="star star-position4"></div>
+    <div class="star star-position5"></div>
+    <div class="star star-position6"></div>
+    <div class="star star-position7"></div>
+  </div>
+  <div class="box-of-star3">
+    <div class="star star-position1"></div>
+    <div class="star star-position2"></div>
+    <div class="star star-position3"></div>
+    <div class="star star-position4"></div>
+    <div class="star star-position5"></div>
+    <div class="star star-position6"></div>
+    <div class="star star-position7"></div>
+  </div>
+  <div class="box-of-star4">
+    <div class="star star-position1"></div>
+    <div class="star star-position2"></div>
+    <div class="star star-position3"></div>
+    <div class="star star-position4"></div>
+    <div class="star star-position5"></div>
+    <div class="star star-position6"></div>
+    <div class="star star-position7"></div>
+  </div>
+  
+    </div>
+  );
+};
+
+export default BackgroundHomeSection;
--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import BackgroundHomeSection from "./BackgroundHomeSection";
 
 const HomeSection = () => {
   return (
     <div className="homeSection" id="homeSection">
-      {/* <h1>Emilien Gerbouin</h1>
-      <h4>Passionné par la création d'interfaces utilisateurs</h4> */}
+      <BackgroundHomeSection />
+      <h1 className="title__home__section">
+        Emilien <br/> Gerbouin
+        {/* <span className="back__title__home__section">Gerbouin <br/> Emilien</span> */}
+      </h1>
+      <h2 className="subtitle__home__section">Passionné par la création d'interfaces utilisateurs</h2>
 
     </div>
   );
