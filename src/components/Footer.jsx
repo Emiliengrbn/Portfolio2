f29962fc7e38ed3865b5e06be5612819f8cd5a4a
Emilien Gerbouin <emilien.gerbouin@gmail.com>
2023-04-01 18:31:54 +0200

    responsive mobile header & footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="coordinate">
-        <div className="email__container">
+        <div className="email__container footer__element">
           <a href="mailto:emilien.gerbouin@gmail.com" className="email__link"> emilien.gerbouin@gmail.com </a>
         </div>
-        <div className="phone__container">
+        <div className="phone__container footer__element">
           <a href="tel:0767901035" className="phoneNumber">07.67.90.10.35</a>
         </div>
-        <div className="localisation__container">
-          <p className="localisation">Pays de la Loire, Bretagne, Normandie</p>
+        <div className="localisation__container footer__element">
+          <p className="localisation">France</p>
         </div>
       </div>
       <p className="copyright">© 2023 - Emilien Gerbouin - Tous droits réservés</p>
